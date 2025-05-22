f3132036884622251a2dc2b6b6acb3e04d9c0bd1
Matheus <matheussales497@gmail.com>
2025-05-22 20:46:09 -0300

    - Refatorada a atividade_01 para adicionar um laço de repetição. - Refatorada a atividade_02 para validar a idade do usuário na escolha do filme e permitir repetir a escolha caso necessário.

--- atividades/atividade_01/main.py
+++ atividades/atividade_01/main.py
@@ -4,28 +4,39 @@ Crie um programa que receba do usuário dois números reais, e uma das 4 operaç
 
 # NOTE - O programa só se encerrará caso o usuário informe isso no programa.
 
-# TODO
+while True:
 
-x = int(input("Coloque o número inteiro x: "))
-y = int(input("Coloque o número inteiro y: "))
+    x = int(input("Coloque o número inteiro x: "))
+    y = int(input("Coloque o número inteiro y: "))
 
-# menu
-print(f"{'-'*20} CALCULADORA PYTHON{'-'*20}")
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão\n")
+    # menu
+    print(f"{'-'*20} CALCULADORA PYTHON{'-'*20}")
+    print("1 - Soma")
+    print("2 - Subtração")
+    print("3 - Multiplicação")
+    print("4 - Divisão\n")
 
-menu = input("Insira a opção desejada: ").strip()
+    menu = input("Insira a opção desejada: ").strip()
 
-match menu:
-    case "1":
-        print(f"A soma dos números é: {x+y}")
-    case "2":
-        print(f"A subratração dos números é: {x-y}")
-    case "3":
-        print(f"A multiplicação dos números é: {x*y}")
-    case "4":
-        print(f"A divisão dos números é: {x/y}")
-    case _:
-        print("Opção inválida")
+    match menu:
+        case "1":
+            print(f"A soma dos números é: {x+y}")
+        case "2":
+            print(f"A subratração dos números é: {x-y}")
+        case "3":
+            print(f"A multiplicação dos números é: {x*y}")
+        case "4":
+            print(f"A divisão dos números é: {x/y}")
+        case _:
+            print("Opção inválida")
+
+    repetir= input("Deseja verificar de novo?  (s/n) ").lower().strip()
+    match repetir:
+        case "s":
+            continue
+        case "n":
+            break
+        case _:
+            print("Opção Inválida.")
+            break
+    break
--- atividades/atividade_02/main.py
+++ atividades/atividade_02/main.py
@@ -11,21 +11,53 @@ O usuário deverá escolher a sala do filme que deseja assistir, e o programa de
 """
 # TODO
 
+# O usuário informa os números
 nome = input("Coloque seu nome: ")
 idade = int(input("Qual sua idade: "))
 
-# menu
-print("Sala 1 - A Roda Quadrada - Livre")
-print("Sala 2 - A Volta dos Que Não Foram - 12 anos")
-print("Sala 3 - Poeira em Alto Mar - 14 anos")
-print("Sala 4 - As tranças do Rei Careca - 16 anos")
-print("Sala 5 - A Vingança do Frango Assado - 18 anos")
+while True:
 
-salas = input("Escolha o número da Sala: ").strip()
+    # menu
+    print("Sala 1 - A Roda Quadrada - Livre")
+    print("Sala 2 - A Volta dos Que Não Foram - 12 anos")
+    print("Sala 3 - Poeira em Alto Mar - 14 anos")
+    print("Sala 4 - As tranças do Rei Careca - 16 anos")
+    print("Sala 5 - A Vingança do Frango Assado - 18 anos")
 
-match salas:
-    case "1":
-        print("")
-    case _:
-        print("Número Incorreto")
+    # O usuário escolhe a operação
+    salas = input("Escolha o número da Sala: ").strip()
+
+    match salas:
+        case "1":
+            print(f"{nome} decidiu assistir A Roda Quadrada - Livre")
+            choice = "A Roda Quadrada"
+
+        case "2": 
+            print(f"{nome} decidiu assistir A Volta dos Que Não Foram - 12 anos" if idade >= 12 else "Idade não permitida. Escolha outro filme")
+            choice = "A Volta dos Que Não Foram"
+
+        case "3":
+            print(f"{nome} decidiu assistir Poeira em Alto Mar - 14 anos" if idade >= 14 else "Idade não permitida. Escolha outro filme")
+            choice = "Poeira em Alto Mar"
+
+        case "4":
+            print(f"{nome} As tranças do Rei Careca - 16 anos" if idade >= 16 else "Idade não permitida. Escolha outro filme")
+            choice = "As tranças do Rei Careca"
+
+        case "5":
+            print(f"{nome} decidiu assistir A Vingança do Frango Assado - 18 anos" if idade >= 18 else "Idade não permitida. Escolha outro filme")
+            choice = "A Vingança do Frango Assado"
+        
+        case _:
+            print("Número da sala incorreto, porfavor escolha um número de 1 a 5")
+    
+    # Pergunta se o usuário deseja escolher outro filme
+    confirmação = input("Deseja escolher outro filme? (s/n): ").strip().lower()
+    if confirmação in ["s", "sim", "ss"]:
+        print("Escolha outro filme, porfavor")
+    elif confirmação in ["n", "não", "nao"]:
+        print(f"Obrigado pela preferência {nome} e por escolher o {choice}, bom filme!!!")
+        break
+    else:        
+        print("Opção inválida. Encerrando o programa.")
         break
