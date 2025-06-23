e8585a97cb14bd64b9f9fed9876af89c8bc21db8
Matheus Sales Carvalho <48875053+MatheusSales2773@users.noreply.github.com>
2025-06-23 17:26:54 -0300

    feat: adicionar funcoes 05

---
+++ funcoes/05_funcoes_parte_05/modulo.py
@@ -0,0 +1,16 @@
+# funcoes
+def somar(x, y):
+    soma = x+y
+    return soma
+
+def substrair(x,y):
+    subtracao = x-y
+    return subtracao
+
+def multiplicar(x,y):
+    multiplicacao = x*y
+    return multiplicacao
+
+def dividir(x,y):
+    divisao = x/y
+    return divisao
